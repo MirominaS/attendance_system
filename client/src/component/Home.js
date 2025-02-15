e02f4483f1b5a7ccdd6858434eb2d64e5b635279
MirominaS <miromin-im19063@stu.kln.ac.lk>
2025-02-15 12:20:35 +0530

    #2.4.2, logout success message added in home

--- client/src/component/Home.js
+++ client/src/component/Home.js
@@ -1,19 +1,27 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Button from './Button';
 import './Home.css'
 import { useNavigate } from 'react-router-dom';
 
+
 const Home = () => {
   const navigate = useNavigate();
+  const [success, setSuccess] = useState('');
 
-  const handleLogout = () => {
-    navigate('/');
-  }
-
+  const handleLogout = (e) =>{
+    e.preventDefault(); 
+    setSuccess('Logout successful! Redirecting to Login...');
+    setTimeout(() => {
+        
+        navigate('/');
+      }, 500);  
+    
+  } 
   return (
     <div className="welcome-container">
       <div className="welcome-box">
         <h1 className="welcome-message">Welcome to the Application!</h1>
+        {success && <p className="success-message">{success}</p>}
         <Button label="Logout" onClick={handleLogout}/>
       </div>
     </div>
