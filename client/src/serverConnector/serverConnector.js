d3166df1a6dd9c4491027c898e3751678aa2530e
MirominaS <miromin-im19063@stu.kln.ac.lk>
2025-02-15 16:58:54 +0530

    #2.6.2, serverConnector improved

--- client/src/serverConnector/serverConnector.js
+++ client/src/serverConnector/serverConnector.js
@@ -1,12 +1,14 @@
 import axios from 'axios';
 
-export const serverConnector = ({url,payload}) => {return new Promise((resolve,reject) => {
-    axios.post(url,payload)
-    .then((response) => {
-        resolve(response);
+export const serverConnector = ({url,payload,headers}) => {return new Promise((resolve,reject) => {
+    let endPointURL = process.env.REACT_APP_API_SERVER_ENDPOINT + url
+        axios.post(endPointURL,payload,{headers:headers})
+            .then((response) => {
+                resolve(response);
+            })
+            .catch((error) => {
+                reject(error?.response?.data)
+            });    
     })
-    .catch((error) => {
-        reject(error?.response?.data)
-      });
-    })
+   
 }
